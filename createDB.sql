540cce7a56a089f963ae2bbb75ece234e965bbff
alexandre <alexandre.laval2000@gmail.com>
2023-03-24 17:24:49 +0100

    correcting sql and inserting test data

--- createDB.sql
+++ createDB.sql
@@ -1,91 +1,90 @@
 DROP DATABASE IF EXISTS Projet;
 CREATE DATABASE Projet;
+
 USE Projet;
 
-CREATE TYPE TypeUtilisateur AS ENUM ('Administrateur', 'Recruteur', 'Candidat');
-CREATE TYPE StatutCompte AS ENUM ('actif', 'inactif', 'bannis');
-
-
-
 CREATE TABLE Organisation (
-  siren VARCHAR(255) NOT NULL,
-  rue VARCHAR(255) NOT NULL,
-  ville VARCHAR(255) NOT NULL,
-  region VARCHAR(255) NOT NULL,
-  codePostal INT NOT NULL,
-  pays VARCHAR(255) NOT NULL,
-  PRIMARY KEY (siren)
+                              siren VARCHAR(255) NOT NULL,
+                              rue VARCHAR(255) NOT NULL,
+                              ville VARCHAR(255) NOT NULL,
+                              region VARCHAR(255) NOT NULL,
+                              codePostal INT NOT NULL,
+                              pays VARCHAR(255) NOT NULL,
+                              PRIMARY KEY (siren)
 );
 
 CREATE TABLE Utilisateur (
-  id INT NOT NULL AUTO_INCREMENT,
-  typeUtilisateur TypeUtilisateur NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  nom VARCHAR(255) NOT NULL,
-  prenom VARCHAR(255) NOT NULL,
-  dateCreation DATE NOT NULL,
-  statutCompte StatutCompte NOT NULL,
-  mdpHash VARCHAR(255) NOT NULL,
-  organisation VARCHAR(255),
-  PRIMARY KEY (id),
-  FOREIGN KEY (organisation) REFERENCES Organisation(siren),
-  CHECK (typeUtilisateur IN ('Administrateur', 'Recruteur', 'Candidat')),
-  CHECK (statutCompte IN ('actif', 'inactif', 'bannis')),
-  CHECK ((typeUtilisateur IN ('Administrateur', 'Candidat') AND organisation IS NULL) OR (typeUtilisateur = 'Recruteur' AND organisation IS NOT NULL))
+                             id INT NOT NULL AUTO_INCREMENT,
+                             typeUtilisateur ENUM('Administrateur', 'Recruteur', 'Candidat') NOT NULL,
+                             email VARCHAR(255) NOT NULL,
+                             nom VARCHAR(255) NOT NULL,
+                             prenom VARCHAR(255) NOT NULL,
+                             dateCreation DATE NOT NULL,
+                             statutCompte ENUM('actif', 'inactif', 'bannis') NOT NULL,
+                             mdpHash VARCHAR(255) NOT NULL,
+                             organisation VARCHAR(255),
+                             PRIMARY KEY (id),
+                             FOREIGN KEY (organisation) REFERENCES Organisation(siren),
+                             CHECK ((typeUtilisateur IN ('Administrateur', 'Candidat') AND organisation IS NULL) OR (typeUtilisateur = 'Recruteur' AND organisation IS NOT NULL))
 );
 
 CREATE TABLE FichePoste (
-  id INT NOT NULL,
-  dateUpload DATE NOT NULL,
-  intitule VARCHAR(255) NOT NULL,
-  responsable VARCHAR(255) NOT NULL,
-  typeMetier VARCHAR(255) NOT NULL,
-  rythme VARCHAR(255) NOT NULL,
-  fourchetteBasse INT NOT NULL,
-  fourchetteHaute INT NOT NULL,
-  description TEXT NOT NULL,
-  PRIMARY KEY (id)
+                            id INT NOT NULL AUTO_INCREMENT,
+                            dateUpload DATE NOT NULL,
+                            intitule VARCHAR(255) NOT NULL,
+                            responsable VARCHAR(255) NOT NULL,
+                            typeMetier VARCHAR(255) NOT NULL,
+                            rythme VARCHAR(255) NOT NULL,
+                            fourchetteBasse INT NOT NULL,
+                            fourchetteHaute INT NOT NULL,
+                            description TEXT NOT NULL,
+                            PRIMARY KEY (id)
 );
 
 CREATE TABLE Offre (
-  numeroOffre INT NOT NULL,
-  dateUpload DATE NOT NULL,
-  type VARCHAR(255) NOT NULL,
-  fichePoste INT NOT NULL,
-  PRIMARY KEY (numeroOffre),
-  FOREIGN KEY (fichePoste) REFERENCES FichePoste(id)
+                       numeroOffre INT NOT NULL AUTO_INCREMENT,
+                       dateUpload DATE NOT NULL,
+                       type VARCHAR(255) NOT NULL,
+                       fichePoste INT NOT NULL,
+                       PRIMARY KEY (numeroOffre),
+                       FOREIGN KEY (fichePoste) REFERENCES FichePoste(id)
 );
 
 CREATE TABLE DossierCandidature (
-  id INT NOT NULL,
-  dateCandidature DATE NOT NULL,
-  statut ENUM('en cours', 'refusé', 'en attente de traitement', 'accepté') NOT NULL,
-  utilisateur INT,
-  offre INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (utilisateur) REFERENCES Utilisateur(id),
-  FOREIGN KEY (offre) REFERENCES Offre(numeroOffre),
-  CHECK (statut IN ('en cours', 'refusé', 'en attente de traitement', 'accepté'))
+                                    id INT NOT NULL AUTO_INCREMENT,
+                                    dateCandidature DATE NOT NULL,
+                                    statut ENUM('en cours', 'refusé', 'en attente de traitement', 'accepté') NOT NULL,
+                                    utilisateur INT,
+                                    offre INT,
+                                    PRIMARY KEY (id),
+                                    FOREIGN KEY (utilisateur) REFERENCES Utilisateur(id),
+                                    FOREIGN KEY (offre) REFERENCES Offre(numeroOffre),
+                                    CHECK (statut IN ('en cours', 'refusé', 'en attente de traitement', 'accepté'))
 );
 
 CREATE TABLE Document (
-  id INT NOT NULL,
-  dateUpload DATE NOT NULL,
-  nom VARCHAR(255) NOT NULL,
-  type VARCHAR(255) NOT NULL,
-  dossierCandidature INT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (dossierCandidature) REFERENCES DossierCandidature(id)
+                          id INT NOT NULL AUTO_INCREMENT,
+                          dateUpload DATE NOT NULL,
+                          nom VARCHAR(255) NOT NULL,
+                          type VARCHAR(255) NOT NULL,
+                          dossierCandidature INT NOT NULL,
+                          PRIMARY KEY (id),
+                          FOREIGN KEY (dossierCandidature) REFERENCES DossierCandidature(id)
 );
 
 CREATE TABLE demandeRecruteur (
-  id INT NOT NULL,
-  dateDemande DATE NOT NULL,
-  statut ENUM('refuse', 'accepte') NOT NULL,
-  organisation VARCHAR(255) NOT NULL,
-  utilisateur INT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (organisation) REFERENCES Organisation(siren),
-  FOREIGN KEY (utilisateur) REFERENCES Utilisateur(id),
-  CHECK (statut IN ('refuse', 'accepte'))
+                                  id INT NOT NULL AUTO_INCREMENT,
+                                  dateDemande DATE NOT NULL,
+                                  statut ENUM('refuse', 'accepte') NOT NULL,
+                                  organisation VARCHAR(255) NOT NULL,
+                                  utilisateur INT NOT NULL,
+                                  PRIMARY KEY (id),
+                                  FOREIGN KEY (organisation) REFERENCES Organisation(siren),
+                                  FOREIGN KEY (utilisateur) REFERENCES Utilisateur(id),
+                                  CHECK (statut IN ('refuse', 'accepte'))
 );
+
+
+
+
+
---
+++ testData.sql
@@ -0,0 +1,34 @@
+
+INSERT INTO Organisation (siren, rue, ville, region, codePostal, pays)
+VALUES ('123456789', '15 Rue de la Paix', 'Paris', 'Ile-de-France', 75001, 'France'),
+       ('987654321', '10 Downing Street', 'London', 'Greater London', 'SW1A 2AA', 'United Kingdom');
+
+INSERT INTO Utilisateur (typeUtilisateur, email, nom, prenom, dateCreation, statutCompte, mdpHash, organisation)
+VALUES ('Administrateur', 'admin@example.com', 'Doe', 'John', '2022-01-01', 'actif', 'passwordhash', NULL),
+       ('Recruteur', 'recruiter@example.com', 'Smith', 'Jane', '2022-01-02', 'actif', 'passwordhash', '123456789'),
+       ('Candidat', 'candidate@example.com', 'Lee', 'Alex', '2022-01-03', 'actif', 'passwordhash', NULL);
+
+INSERT INTO FichePoste (dateUpload, intitule, responsable, typeMetier, rythme, fourchetteBasse, fourchetteHaute, description)
+VALUES ('2022-01-01', 'Développeur Full-Stack', 'Doe John', 'Informatique', 'CDI', 35000, 45000, 'Nous cherchons un développeur Full-Stack expérimenté pour rejoindre notre équipe dynamique.'),
+       ('2022-01-02', 'Chef de projet marketing', 'Smith Jane', 'Marketing', 'CDI', 40000, 50000, 'Nous cherchons un chef de projet marketing passionné pour mener notre équipe de marketing.'),
+       ('2022-01-03', 'Assistant administratif', 'Lee Alex', 'Administration', 'CDD', 20000, 25000, 'Nous cherchons un assistant administratif à temps partiel pour soutenir nos opérations quotidiennes.');
+
+INSERT INTO Offre (dateUpload, type, fichePoste)
+VALUES ('2022-01-01', 'CDI', 1),
+       ('2022-01-02', 'CDI', 2),
+       ('2022-01-03', 'CDD', 3);
+
+INSERT INTO DossierCandidature (dateCandidature, statut, utilisateur, offre)
+VALUES ('2022-01-05', 'en cours', 3, 1),
+       ('2022-01-06', 'en cours', 3, 2),
+       ('2022-01-07', 'en attente de traitement', 3, 3);
+
+INSERT INTO Document (dateUpload, nom, type, dossierCandidature)
+VALUES ('2022-01-05', 'CV', 'PDF', 1),
+       ('2022-01-05', 'Lettre de motivation', 'PDF', 1),
+       ('2022-01-06', 'CV', 'PDF', 2),
+       ('2022-01-06', 'Lettre de motivation', 'PDF', 2);
+
+INSERT INTO demandeRecruteur (dateDemande, statut, organisation, utilisateur)
+VALUES ('2022-01-01', 'accepte', '123456789', 2),
+       ('2022-01-02', 'refuse', '987654321', 2);
