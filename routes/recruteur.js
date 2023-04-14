1ac79d4671af22f98aa7e7bcf7e6815abd1655b2
pierre adorni <pierreadorni@free.fr>
2023-04-14 18:32:48 +0200

    recruteur route file

---
+++ routes/recruteur.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const FichePoste = require('../models/FichePoste');
+
+router.get('/', (req, res) => {
+    res.redirect('offres')
+})
+
+router.get('/offres', (req, res) => {
+    // if url parameter 'sforsearch' is not empty, then search for offers
+    // if(req.query.sforsearch){
+    //     FichePoste.search(req.query.sforsearch, (err, result) => {
+    //         res.render('offersList', {fichesPostes: result, query: req.query.sforsearch});
+    //     })
+    // }
+    FichePoste.readAllForOrganisation(req.session.user.organisation, (err, result) => {
+        res.render('recruteur/offersList', {fichesPostes: result});
+    })
+})
+
+router.get('/offres/:id', (req, res) => {
+    FichePoste.read(req.params.id, (err, result) => {
+        res.render('candidat/offer', {fichePoste: result});
+    })
+})
+
+module.exports = router;
