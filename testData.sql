4ff5ca7d37dd5736edd7b4d19253e1945064b9d7
LAVAL Alexandre <70696783+MasterTrtle@users.noreply.github.com>
2023-06-14 16:13:01 +0200

    Add filter requests and applications for the recruiter

--- testData.sql
+++ testData.sql
@@ -7,7 +7,9 @@ INSERT INTO Utilisateur (typeUtilisateur, email, nom, prenom, dateCreation, stat
 VALUES ('Administrateur', 'admin@example.com', 'Doe', 'John', '2022-01-01', 'actif', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', NULL),
        ('Recruteur', 'recruiter@example.com', 'Smith', 'Jane', '2022-01-02', 'actif', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '123456789'),
        ('Candidat', 'candidate@example.com', 'Lee', 'Alex', '2022-01-03', 'actif', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', NULL),
-       ('Candidat', 'bientotRecruteur@example.com', 'Mama', 'Joe', '2022-01-03', 'actif', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', NULL);
+       ('Candidat', 'bientotRecruteur@example.com', 'Mama', 'Joe', '2022-01-03', 'actif', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', NULL),
+       ('Candidat', 'candidate2@example.com', 'anon', 'john', '2023-01-03', 'actif', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', NULL);
+
 
 
 INSERT INTO FichePoste (organisation, dateUpload, intitule, responsable, typeMetier, rythme, fourchetteBasse, fourchetteHaute, description, localisation)
@@ -22,6 +24,9 @@ VALUES ('2022-01-01', 'CDI', 1),
 
 INSERT INTO DossierCandidature (dateCandidature, statut, utilisateur, offre)
 VALUES ('2022-01-05', 'refusé', 3, 1),
+       ('2022-01-07', 'en attente de traitement', 5, 1),
+       ('2022-01-07', 'en attente de traitement', 5, 2),
+       ('2022-01-07', 'en attente de traitement', 5, 3),
        ('2022-01-06', 'accepté', 3, 2),
        ('2022-01-07', 'en attente de traitement', 3, 3);
 
