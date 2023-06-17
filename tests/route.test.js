90e7d637e1fafbd3b442da66c068a0ad245dce5d
LAVAL Alexandre <70696783+MasterTrtle@users.noreply.github.com>
2023-06-17 20:45:18 +0200

    test for account registration

--- tests/route.test.js
+++ tests/route.test.js
@@ -3,6 +3,7 @@
 const request = require("supertest");
 const app = require("../app");
 const sha256 = require("sha256");
+const Utilisateur = require("../models/utilisateur");
 
 describe("Test the root path", () => {
     test("GET /", (done) => {
@@ -17,23 +18,23 @@ describe("Test the root path", () => {
     test("POST /login", (done) => {
         request(app)
             .post("/login")
-            .send({ email: "admin@example.com", password: "123456" })
+            .send({email: "admin@example.com", password: "123456"})
             .then((response) => {
                 expect(response.statusCode).toBeLessThan(400);
                 expect(response.header.location).toBe("/");
                 request(app).get("/")
                     .set('Cookie', response.header['set-cookie'])
                     .then((response) => {
-                    expect(response.header.location).toBe("/admin/users");
-                    done();
-                });
+                        expect(response.header.location).toBe("/admin/users");
+                        done();
+                    });
             })
             .catch((error) => done(error));
     });
     test("POST /login with wrong password", (done) => {
         request(app)
             .post("/login")
-            .send({ email: "admin@example.com", password: "cakeisalie" })
+            .send({email: "admin@example.com", password: "cakeisalie"})
             .then((response) => {
                 expect(response.statusCode).toBe(401);
                 done();
@@ -43,7 +44,7 @@ describe("Test the root path", () => {
     test("POST /login with wrong email", (done) => {
         request(app)
             .post("/login")
-            .send({ email: "admin@example", password: "123456" })
+            .send({email: "admin@example", password: "123456"})
             .then((response) => {
                 expect(response.statusCode).toBe(401);
                 done();
@@ -53,24 +54,31 @@ describe("Test the root path", () => {
     test("POST /register", (done) => {
         const userData = {
             email: "test@example.com",
-            password: ("123456"),
+            password: "123456",
             lastname: "John",
             firstname: "Anon",
         };
         request(app)
-    .post("/register")
-    .send(userData)
-    .then((response) => {
-        expect(response.statusCode).toBeLessThan(400);
-        done();
-    })
-    .catch((error) => done(error));
+            .post("/register")
+            .send(userData)
+            .then((response) => {
+                expect(response.statusCode).toBeLessThan(400);
+                //check if user is in database
+                Utilisateur.readfromMail(userData.email)
+                    .then((result) => {
+                        expect(result.email).toBe(userData.email);
+                        expect(result.nom).toBe(userData.lastname);
+                        expect(result.prenom).toBe(userData.firstname);
+                        done();
+                    })
+                    .catch((error) => done(error));
+            })
+            .catch((error) => done(error));
     });
-
     afterAll(() => {
         // Perform any necessary cleanup or closing operations here
         // For example, closing database connections or server instances
-
+        Utilisateur.deletefromMail("test@example.com");
     });
 });
 
